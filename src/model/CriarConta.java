87ab215069c49ccaa15d99f27f1960cb8d1f6f89
alvaro.carvalho.703 <alvaro.carvalho.703@ufrn.edu.br>
2022-09-09 15:25:53 -0300

    init

---
+++ src/model/CriarConta.java
@@ -0,0 +1,9 @@
+package model;
+
+public class CriarConta {
+    public static void main(String[] args) {
+        Conta primeiraConta = new Conta();
+        primeiraConta.saldo = 200;
+        System.out.println(primeiraConta.saldo);
+    }
+}
