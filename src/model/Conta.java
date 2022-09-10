738e34a7bbac7ff92d2275b4431f1d96be1a4ca2
alvaro.carvalho.703 <alvaro.carvalho.703@ufrn.edu.br>
2022-09-10 13:40:04 -0300

    tests

--- src/model/Conta.java
+++ src/model/Conta.java
@@ -1,8 +1,24 @@
 package model;
 
- public class Conta {
+import java.util.UUID;
+
+public class Conta {
     public double saldo;
-    int agencia;
+    public int agencia;
     int numero;
     String titular;
+
+    public String deposita(double valor) {
+        this.saldo += valor;
+        return "Depósito realizado com sucesso. Seu ticket é: " + UUID.randomUUID().toString();
+    }
+    public boolean saca(double valor) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
